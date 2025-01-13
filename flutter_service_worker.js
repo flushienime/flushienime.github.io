7dc60b8fd188c30e7b365026226027ee4d93702d
Cao Hữu Minh Trung <caohuuminhtrung303@gmail.com>
2025-01-13 23:08:44 +0700

    sidebar update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "c57d6713cdcfcc2fedc9f4a01f1fec8b",
-"assets/AssetManifest.bin.json": "385ae428ec4ab635ec5274abc26f0e4a",
-"assets/AssetManifest.json": "1426220c9991f42518645c1bc4259eea",
+const RESOURCES = {"assets/AssetManifest.bin": "22f994caf4a141b4611de3b21414468c",
+"assets/AssetManifest.bin.json": "922a0a0dad69d84290301f9120ec01b2",
+"assets/AssetManifest.json": "dfcbb23412005bb1b5ba83efde6326be",
 "assets/assets/audio/Goi_dau.mp3": "96ab9133aa1a3cf7eacdff569f3f07b1",
 "assets/assets/audio/Goi_dau_2.mp3": "5a985f3f6e7573929a0a70fb439741e0",
 "assets/assets/audio/Goi_dau_3.mp3": "96e7ca0233dc4797313b1ad2fe7b413d",
@@ -17,8 +17,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "c57d6713cdcfcc2fedc9f4a01f1fec8b
 "assets/assets/audio/Ko_goi_dau_3.mp3": "de114b73f05eb294cb5fc3205c31bd64",
 "assets/assets/audio/Ko_goi_dau_4.mp3": "d71b4f6454d972425a8a0e7240e1682c",
 "assets/assets/audio/Ko_goi_dau_5.mp3": "fe794391d1bbbc43c82538c9fddc9e2f",
+"assets/assets/image/MTea_image.jpg": "6921f49fac40f12e0973dd5aa96de3e4",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "f22f29c588079a435cb81660a0208e5e",
+"assets/fonts/MaterialIcons-Regular.otf": "74c441fec60f5a451eceecc4b1cde3ff",
 "assets/NOTICES": "dcaa60336d23f59f4fcd26466f303d28",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
@@ -34,14 +35,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "c57d6713cdcfcc2fedc9f4a01f1fec8b
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "00d908d269802d17d18061c117c6ee9e",
+"flutter_bootstrap.js": "42faed048936fa1ca1c30e31e9cfb207",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "a4444f605742e7fa6b47194810f6961f",
-"/": "a4444f605742e7fa6b47194810f6961f",
-"main.dart.js": "1cc89fa33d5cbdcc560b2703199a132a",
+"index.html": "36a9b22d3a27a6e0fc987525f052b96e",
+"/": "36a9b22d3a27a6e0fc987525f052b96e",
+"main.dart.js": "aa30b8ad6282ab426c9690dbb5134a87",
 "manifest.json": "72e1c0918265371eceb0115e19d8875b",
 "version.json": "a017b68b91250e13849cdec169b3879d"};
 // The application shell files that are downloaded before a service worker can
